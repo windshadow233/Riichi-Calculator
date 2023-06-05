2bc315fa0a9ca58c6849c4dc2550324578536bb6
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2023-06-05 17:08:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from st_pages import show_pages, Page
-# menu = ["计算器", "役种一览"]
-# choice = st.sidebar.selectbox("Menu", menu, label_visibility='collapsed')
-# if choice == "计算器":
-#     calculator_ui()
 
 show_pages(
     [
